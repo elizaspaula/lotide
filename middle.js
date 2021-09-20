48448d7b8c109c731472732ed1af803092a26290
Elizabete Silva de Paula <elizabete.silvapaula@gmail.com>
2021-09-20 22:51:16 +0000

    Added export module

--- middle.js
+++ middle.js
@@ -1,9 +1,7 @@
-//The middle function should return an array with only the middle element(s) of the provided array. This means that the length of the returned elements could vary.
-
-// For arrays with one or two elements, there is no middle. Return an empty array
-
+//The middle function should return an array with only the middle element(s) of the provided array.
+//This means that the length of the returned elements could vary.
+//For arrays with one or two elements, there is no middle. Return an empty array
 //For arrays with odd number of elements, an array containing a single middle element should be returned
-
 //For arrays with an even number of elements, an array containing the two elements in the middle should be returned
 
 const middle = function (array) {
@@ -19,3 +17,5 @@ const middle = function (array) {
 console.log(middle([1, 2, 3, 4])); // expect result should be 2,3
 console.log(middle([1, 2])); //  expect result should array = []
 console.log(middle([1, 3, 4])); // a single element should returned
+
+module.exports = middle;
