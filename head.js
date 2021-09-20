f35be8e3919b6daf133d1a4bcac222f104ea5103
Elizabete Silva de Paula <elizabete.silvapaula@gmail.com>
2021-09-20 23:08:33 +0000

    Add export module

--- head.js
+++ head.js
@@ -1,13 +1,7 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertation Passed ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertation Failed ${actual} !== ${expected}`);
-  }
-};
-
 const head = function (myArray) {
   return myArray[0];
 };
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
