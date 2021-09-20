750df587660a0c993be57dbe5a1cc25b87ecab7b
Elizabete Silva de Paula <elizabete.silvapaula@gmail.com>
2021-09-20 21:11:38 +0000

    Refactor Head function with Mocha & Chai

---
+++ test/headTest.js
@@ -0,0 +1,19 @@
+const assert = require("chai").assert;
+const head = require("../head");
+
+describe("#head", () => {
+  it("should returns 1 for [1, 2, 3]", () => {
+    const inputArray = [1, 2, 3];
+    assert.strictEqual(head(inputArray), 1);
+  });
+
+  it("should returns 5 for [5, 6, 7]", () => {
+    const inputArray = [5, 2, 7];
+    assert.strictEqual(head(inputArray), 5);
+  });
+
+  it('should returns Hello for ["Hello", "Lighthouse", "Labs"]', () => {
+    const inputArray = ["Hello", "Lighthouse", "Labs"];
+    assert.strictEqual(head(inputArray), "Hello");
+  });
+});
